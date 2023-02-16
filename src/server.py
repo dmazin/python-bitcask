003fd8ee5924cb78537d7a1f6b012c15c7ea6ff7
Dmitry Mazin <dm@archvile.net>
2023-02-16 11:33:39 +0000

    handle POST requests

--- src/server.py
+++ src/server.py
@@ -1,6 +1,6 @@
 import http.server
 import json
-from typing import Optional, Tuple
+from typing import Dict, Optional, Tuple
 import urllib.parse
 
 import constants
@@ -10,9 +10,9 @@ import db
 class MyHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self) -> None:
         """
-        Expects a request like `GET /?key=foo`
+        Expects a request like `curl http:/?key=foo`
         """
-        parsed_url = urllib.parse.urlparse(self.path)
+        parsed_url: urllib.parse.ParseResult = urllib.parse.urlparse(self.path)
         qs: dict[str, list[str]] = urllib.parse.parse_qs(parsed_url.query)
         key: Optional[str] = qs.get('key', [None])[0]
         if key:
@@ -30,7 +30,26 @@ class MyHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             self.end_headers()
 
     def do_POST(self) -> None:
-        pass
+        """
+        Expects a request like `curl -X POST --json '{"foo": "bar", "bar": "baz"}' http:/`
+        """
+        content_length: int = int(self.headers.get('Content-Length', 0))
+        if content_length == 0:
+            self.send_response(400)
+            self.end_headers()
+
+        post_body_bytes: bytes = self.rfile.read(content_length)
+        post_body: Dict = json.loads(post_body_bytes.decode())
+
+        # TODO This is a little cheeky. Instead, be boring and just take a dict
+        # that looks like {"key": "foo", "value": "bar"}.
+        key: str
+        value: str
+        for key, value in post_body.items():
+            db.set(key, value)
+
+        self.send_response(201)
+        self.end_headers()
 
 
 if __name__ == "__main__":
